b6a9397baba78853c45fe522a8bfd7f8abcfe17f
pedro-donoso <pedro.donoso.r@usach.cl>
2021-01-24 18:46:58 -0300

    orilla

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 //base
-const map = L.map('map', { zoomControl: false }).setView([-33.5, -70.5], 10);
+const map = L.map('map', { zoomControl: false }).setView([-33.5, -70.0], 10);
 const title = L.control();
 title.onAdd = function (map) {
     const div = L.DomUtil.create('div', 'info');
