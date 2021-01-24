32adb9033713f4ae0ec46244c85f7ef056153912
pedro-donoso <pedro.donoso.r@usach.cl>
2021-01-24 18:38:00 -0300

    lado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 //base
-const map = L.map('map', { zoomControl: false }).setView([-33.60, -70.60], 14);
+const map = L.map('map', { zoomControl: false }).setView([-33.60, -70.58], 14);
 const title = L.control();
 title.onAdd = function (map) {
     const div = L.DomUtil.create('div', 'info');
