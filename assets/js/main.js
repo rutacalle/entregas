1ea722c820914b1d973b9f68724c9f06ac741dbc
pedro-donoso <pedro.donoso.r@usach.cl>
2021-01-24 18:05:29 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 //base
-const map = L.map('map', { zoomControl: false }).setView([-33.5210, -70.5050], 10);
+const map = L.map('map', { zoomControl: false }).setView([-33.53, -70.53], 10);
 const title = L.control();
 title.onAdd = function (map) {
     const div = L.DomUtil.create('div', 'info');
