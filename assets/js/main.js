73720de739cd174f2ce9b1fe9095c60128164b2f
pedro-donoso <pedro.donoso.r@usach.cl>
2021-01-25 16:45:18 -0300

    correcta definicion

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,37 +70,37 @@ map.addLayer(rejas);
 
 //textos
 point = L.marker([-33.612367, -70.572543]).addTo(map).bindPopup
-    ("<center><b>Punto 1/11 (elena)</b></center> <li>Santa Elena con Eyzaguirre</li><li>Orilla muro area verde.</li><li>3 tixs.</li>");
+    ("<center><b>ELENA</b></center> <li>Santa Elena con Eyzaguirre</li><li>Orilla muro area verde.</li><li>3 tixs.</li>");
 
 point = L.marker([-33.612422, -70.573534]).addTo(map).bindPopup
-    ("<center><b>Punto 2/11 (tottus)</b></center> <li>Estacionamiento Subterraneo.</li><li>8 tixs.</li><li>Coordinar limpieza.</li>");
+    ("<center><b>SUBTERRANEO</b></center> <li>Estacionamiento Subterraneo.</li><li>8 tixs.</li><li>Coordinar limpieza.</li>");
 
 point = L.marker([-33.609554, -70.575515]).addTo(map).bindPopup
-    ("<center><b>Punto 3/11 (puente)</b></center> <li>Plaza de Puente.</li><li>Alta convocatoria.</li><li>Entre 10 a 20 tixs</li>");
+    ("<center><b>PUENTE</b></center> <li>Plaza de Puente Alto.</li><li>Gran convocatoria.</li><li>Entre 10 a 20 tixs</li>");
 
 point = L.marker([-33.608687, -70.577718]).addTo(map).bindPopup
-    ("<center><b>Punto 4/11 (fruna)</b></center> <li>Cuidadores autos</li><li>5 a 7 tixs</li>");
+    ("<center><b>ESTACIONAMIENTOS</b></center> <li>Estacionamientos Fruna</li><li>Cuidadores autos</li><li>5 a 7 tixs</li>");
 
 point = L.marker([-33.60781, -70.57882]).addTo(map).bindPopup
-    ("<center><b>Punto 5/11 (matte)</b></center> <li>Plaza La Matte.</li><li>Cocina Red, Barbería.</li><li>3 a 5 tixs aprox.</li>");
+    ("<center><b>MATTE</b></center> <li>Plaza La Matte.</li><li>Cocina Red, Barbería.</li><li>3 a 5 tixs aprox.</li>");
 
 point = L.marker([-33.60355, -70.57611]).addTo(map).bindPopup
-    ("<center><b>Punto 6/11 (paz)</b></center> <li>Plaza La Paz</li><li>Actividad barberia.</li><li>4 a 6 tixs aprox.</li>");
+    ("<center><b>PAZ</b></center> <li>Plaza La Paz</li><li>Actividad barberia.</li><li>4 a 6 tixs aprox.</li>");
 
 point = L.marker([-33.576906, -70.581498]).addTo(map).bindPopup
-    ("<center><b>Punto 7/11 (sotero)</b></center>  <li>Hospital Sotero del Rio.</li><li>Area verde.</li><li>3 tixs aprox.</li>");
+    ("<center><b>SOTERO</b></center>  <li>Hospital Sotero del Rio.</li><li>Area verde.</li><li>3 tixs aprox.</li>");
 
 point = L.marker([-33.577262, -70.582232]).addTo(map).bindPopup
-    ("<center><b>Punto 8/11 (bandejon)</b></center> <li>Bandejon central</li><li>Detras estacion Metro</li><li>3 tixs aprox.</li><li>1 carpa.</li>");
+    ("<center><b>BANDEJON</b></center> <li>Bandejon central</li><li>Detras estacion Metro</li><li>3 tixs aprox.</li><li>1 carpa.</li>");
 
 point = L.marker([-33.576219, -70.582849]).addTo(map).bindPopup
-    ("<center><b>Punto 9/11 (jehova)</b></center> <li>Esquina templo</li><li>1 carpa</li><li>1 tix.</li>");
+    ("<center><b>JEHOVA</b></center> <li>Esquina templo</li><li>1 carpa</li><li>1 tix.</li>");
 
 point = L.marker([-33.614244, -70.57489]).addTo(map).bindPopup
-    ("<center><b>Punto 10/11 (poderoso)</b></center> <li>Frente a El Poderoso</li><li>2 rucos.</li><li>referencia escultura</li><li>3 tixs aprox.</li>");
+    ("<center><b>PODEROSO</b></center> <li>Frente a El Poderoso</li><li>2 rucos.</li><li>Referencia escultura</li><li>3 tixs aprox.</li>");
 
 point = L.marker([-33.595561, -70.578706]).addTo(map).bindPopup
-    ("<center><b>Punto 11/11</b></center> <li>Frente a Municipalidad.</li><li>Bajo metro, entre rejas.</li><li>1 tix (esporadico)</li>");
+    ("<center><b>REJAS</b></center> <li>Frente a Municipalidad.</li><li>Bajo metro, entre rejas.</li><li>1 tix (esporadico)</li>");
 
 //zoom
 const osm2 = new L.TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { minZoom: 0, maxZoom: 18 });
