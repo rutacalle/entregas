7cb831513c9e663b5e5171c87c5168a8b1b53ae1
pedro-donoso <pedro.donoso.r@usach.cl>
2021-01-25 16:57:52 -0300

    coordenadas
    correccion

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,7 +104,7 @@ point = L.marker([-33.614244, -70.57489]).addTo(map).bindPopup
 point = L.marker([-33.595561, -70.578706]).addTo(map).bindPopup
     ("<center><b>REJAS</b></center> <li>Frente a Municipalidad.</li><li>Bajo metro, entre rejas.</li><li>1 tix (esporadico)</li>");
 
-point = L.marker([-33.595561, -70.578706]).addTo(map).bindPopup
+point = L.marker([-33.599432, -70.556685]).addTo(map).bindPopup
     ("<center><b>BALLENA</b></center> <li>Cerro La Ballena.</li><li>1 ruco.</li><li>4 tixs.</li><li>Bajan con termo.</li>");
 
 //zoom
