0e96ed30b22295084c953fec1cb6dee4dbd4a3c3
pedro-donoso <pedro.donoso.r@usach.cl>
2021-01-29 18:18:20 -0300

    marcadores

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,8 @@ const title = L.control();
 title.onAdd = function (map) {
     const div = L.DomUtil.create('div', 'info');
     div.innerHTML +=
-        '<h6>Puntos de entrega</h6> Desayunos, Almuerzos y Ruta noche';
+        '<h6>Puntos de entrega</h6>Rojo = ruco, carpa, colchon';
+     
 
     return div;
 };
@@ -98,7 +99,7 @@ blue, gold, red, green, orange, yellow, violet, grey, black
 */
 
 
-var marcador = new L.Icon({
+var gold = new L.Icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     iconSize: [25, 41],
@@ -107,46 +108,55 @@ var marcador = new L.Icon({
     shadowSize: [41, 41]
 });
 
+var red = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
 //marcadores y textos
-const elena = L.marker([-33.612367, -70.572543], { icon: marcador }).addTo(map).bindPopup
-    ("<center><b>ELENA</b></center> <li>Santa Elena con Eyzaguirre</li><li>Orilla muro area verde.</li><li>3 tixs.</li>");
+const elena = L.marker([-33.612367, -70.572543], { icon: red }).addTo(map).bindPopup
+    ("<center><b>ELENA</b></center> <li>Santa Elena con Eyzaguirre</li><li>2 colchones.</li><li>Orilla muro area verde.</li><li>3 tixs.</li>");
 
-const tottus = L.marker([-33.612422, -70.573534], { icon: marcador }).addTo(map).bindPopup
+const tottus = L.marker([-33.612422, -70.573534], { icon: red }).addTo(map).bindPopup
 
-    ("<center><b>SUBTERRANEO</b></center> <li>Estacionamiento Tottus.</li><li>8 tixs.</li><li>Coordinar limpieza.</li>");
+    ("<center><b>SUBTERRANEO</b></center> <li>Estacionamiento Tottus.</li><li>8 tixs, colchones</li><li>Coordinar limpieza.</li>");
 
 
-const puente = L.marker([-33.609554, -70.575515], { icon: marcador }).addTo(map).bindPopup
+const puente = L.marker([-33.609554, -70.575515], { icon: gold }).addTo(map).bindPopup
 
     ("<center><b>PUENTE</b></center> <li>Plaza de Puente Alto.</li><li>2 diabeticos.</li><li>Entre 10 a 20 tixs</li>");
 
-const fruna = L.marker([-33.608687, -70.577718], { icon: marcador }).addTo(map).bindPopup
+const fruna = L.marker([-33.608687, -70.577718], { icon: gold }).addTo(map).bindPopup
 
     ("<center><b>ESTACIONAMIENTOS</b></center> <li>Estacionamientos Fruna</li><li>Cuidadores autos</li><li>5 a 7 tixs</li>");
 
-const matte = L.marker([-33.60781, -70.57882], { icon: marcador }).addTo(map).bindPopup
+const matte = L.marker([-33.60781, -70.57882], { icon: gold }).addTo(map).bindPopup
 
     ("<center><b>MATTE</b></center> <li>Plaza La Matte.</li><li>Cocina Red.</li></li><li>Actividad Barbería.</li><li>3 a 5 tixs aprox.</li>");
 
-const paz = L.marker([-33.60355, -70.57611], { icon: marcador }).addTo(map).bindPopup
-    ("<center><b>PAZ</b></center> <li>Plaza La Paz</li><li>Actividad Barberia.</li><li>4 a 6 tixs aprox.</li>");
+const paz = L.marker([-33.60355, -70.57611], { icon: red }).addTo(map).bindPopup
+    ("<center><b>PAZ</b></center> <li>Plaza La Paz</li><li>2 rucos, 3 colchones.</li><li>4 a 6 tixs aprox.</li>");
 
-const sotero = L.marker([-33.576906, -70.581498], { icon: marcador }).addTo(map).bindPopup
+const sotero = L.marker([-33.576906, -70.581498], { icon: gold }).addTo(map).bindPopup
     ("<center><b>SOTERO</b></center>  <li>Hospital Sotero del Rio.</li><li>Area verde.</li><li>3 tixs aprox.</li>");
 
-const bandejon = L.marker([-33.577262, -70.582232], { icon: marcador }).addTo(map).bindPopup
+const bandejon = L.marker([-33.577262, -70.582232], { icon: red }).addTo(map).bindPopup
     ("<center><b>BANDEJON</b></center> <li>Bandejon central</li><li>Detras estacion Metro</li><li>3 tixs aprox.</li><li>1 carpa.</li>");
 
-const jehova = L.marker([-33.576219, -70.582849], { icon: marcador }).addTo(map).bindPopup
-    ("<center><b>JEHOVA</b></center> <li>Esquina templo</li><li>1 carpa</li><li>1 tix.</li>");
+const jehova = L.marker([-33.576219, -70.582849], { icon: red }).addTo(map).bindPopup
+    ("<center><b>JEHOVA</b></center> <li>Esquina templo.</li><li>1 carpa.</li><li>1 tix.</li>");
 
-const poderoso = L.marker([-33.614244, -70.57489], { icon: marcador }).addTo(map).bindPopup
+const poderoso = L.marker([-33.614244, -70.57489], { icon: red }).addTo(map).bindPopup
     ("<center><b>PODEROSO</b></center> <li>Frente a El Poderoso</li><li>2 rucos.</li><li>Referencia escultura</li><li>3 tixs aprox.</li>");
 
-const rejas = L.marker([-33.595561, -70.578706], { icon: marcador }).addTo(map).bindPopup
-    ("<center><b>REJAS</b></center> <li>Frente a Municipalidad.</li><li>Bajo metro, entre rejas.</li><li>1 tix (esporadico)</li>");
+const rejas = L.marker([-33.595561, -70.578706], { icon: red }).addTo(map).bindPopup
+    ("<center><b>REJAS</b></center> <li>Frente a Municipalidad.</li><li>1 colchon.</li><li>Bajo metro, enrejado.</li><li>1 tix (esporadico)</li>");
 
-const ballena = L.marker([-33.59934, -70.556156], { icon: marcador }).addTo(map).bindPopup
+const ballena = L.marker([-33.59934, -70.556156], { icon: red }).addTo(map).bindPopup
     ("<center><b>BALLENA</b></center> <li>Cerro La Ballena.</li><li>1 ruco.</li><li>4 tixs.</li><li>Bajan con termo.</li>");
 
 
